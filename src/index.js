25dca74f46f8f159cd0b789ce1aa96e1c734b231
gustavo <gus09ayala@gmail.com>
2022-10-14 23:12:52 -0300

    Created event listener on DOMContentLoader for mem

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import * as Dom from './domModules'
 
+window.addEventListener('DOMContentLoaded', Dom.loadLocalStorage)
+
+
 const newToDoBtn = document.getElementById('new__toDo-btn');
 newToDoBtn.addEventListener('click', Dom.createCard)
 
